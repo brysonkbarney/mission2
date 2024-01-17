8e37483a11fd6865cf6e6572c8c628522c2457c4
brysonbarney <brysonbarney@BRYSONBARNEBD95>
2024-01-17 13:31:12 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m2
+{
+    internal class DiceRoller
+    {
+        public int[] RollDice(int numberOfRolls)
+        {
+            int[] rollFrequencies = new int[13]; // Array to store frequencies of roll sums
+
+            Random random = new Random(); // Random number generator for dice rolls
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                // Roll two dice and sum them up
+                int sum = random.Next(1, 7) + random.Next(1, 7);
+
+                // Increment the frequency of the resulting sum
+                rollFrequencies[sum]++;
+            }
+
+            return rollFrequencies;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace m2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial welcome message and user prompt for the number of dice rolls
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+            // Reading and parsing the user's input
+            int numberOfRolls = int.Parse(Console.ReadLine());
+
+            // Simulating dice rolls
+            DiceRoller dr = new DiceRoller();
+            int[] frequencies = dr.RollDice(numberOfRolls);
+
+            // Processing and displaying the results
+            for (int i = 2; i <= 12; i++)
+            {
+                double percentage = (double)frequencies[i] / numberOfRolls * 100;
+                Console.Write($"{i}: ");
+
+                // Displaying the frequency of each roll as asterisks
+                for (int j = 0; j < (int)percentage; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Closing message
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
